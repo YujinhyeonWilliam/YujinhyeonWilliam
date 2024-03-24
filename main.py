44cc23842715fd7cba136200db42f9f99cd0ea08
YuJinhyeon <148223521+YujinhyeonWilliam@users.noreply.github.com>
2024-03-24 22:31:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,43 @@ RSS_FEED = feedparser.parse(URL)
 MAX_POST=7
 
 markdown_text = """
-[Recent Blog Posts]
+<img src="https://capsule-render.vercel.app/api?type=waving&color=BDBDC8&height=150&section=header" />
+<div align=center>
+ 
+[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2FYujinhyeonWilliam%2F&count_bg=%23EF9605&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)
+  
+</div>
+
+### Thanks For Coming
+
+- ⚡ Former Unity Game Developer (2yrs)
+
+![Jinhyeon's GitHub Stats](https://github-readme-stats.vercel.app/api?username=YujinhyeonWilliam&show_icons=true&theme=vision-friendly-dark) 
+
+![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=YujinhyeonWilliam&layout=compact&theme=vision-friendly-dark)
+
+<br/>
+
+## 📚 Currently Studying
+<img src="https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white"> <img src="https://img.shields.io/badge/unrealengine-%23313131.svg?style=for-the-badge&logo=unrealengine&logoColor=white"> <img src="https://img.shields.io/badge/AWS-2B283A.svg?style=for-the-badge&logo=amazon-aws&logoColor=white"> 
+
+<br/>
+
+## 🔧 Skills Available
+#### [Main]
+<img src="https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=csharp&logoColor=white"> <img src="https://img.shields.io/badge/unity-%23000000.svg?style=for-the-badge&logo=unity&logoColor=white">
+
+#### [Sub]
+<img src="https://img.shields.io/badge/firebase-a08021?style=for-the-badge&logo=firebase&logoColor=ffcd34"> <img src="https://img.shields.io/badge/BigQuery-005571?style=for-the-badge&logo=googlebigquery"> <img src="https://img.shields.io/badge/Google Analytics-414141?style=for-the-badge&logo=googleanalytics"> 
+
+<br/>
+
+## ✍ Recent Blog Posts
+<div style="display:flex; flex-direction:row;">
+    <a href="https://yjhdevelopdiary.tistory.com/">
+        <img src="https://img.shields.io/badge/Tistory-000000?style=for-the-badge&logo=Tistory&logoColor=white"> 
+    </a> <br/>
+
 """ # list of blog posts will be appended here
 
 
@@ -19,3 +55,5 @@ for idx, feed in enumerate(RSS_FEED['entries']):
 f = open("README.md",mode="w", encoding="utf-8")
 f.write(markdown_text)
 f.close()
+
+<img src="https://capsule-render.vercel.app/api?type=waving&color=BDBDC8&height=150&section=footer" />
